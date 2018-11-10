1f657ba04ed9928738dba37cf02b62c20c392703
Jakob Bellamy <jakbellamy@gmail.com>
2018-11-10 17:34:16 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,14 +26,13 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["whale bone corsets"] = 5
+  shipping_manifest["porcelain vases"] = 2
+  shipping_manifest["oil paintings"] = 3
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4]
 
+shipping_manifest
 
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
 
 end
